5830897c1da0450cbb7f4ed452f025bb14ff682c
Jan Schneider <st117301@stud.uni-stuttgart.de>
2020-01-18 22:06:44 +0100

    Tried to establish a compose setup which uses a container as demo device. However, a SSH connection cannot be established

---
+++ demo_device/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting software components..."
+service ssh restart
+tail -f /dev/null
---
+++ full/docker-push.sh
@@ -0,0 +1,3 @@
+docker build -t ipvsas/mbp:latest .
+docker login -u schneijan -p lunchonalamb
+docker push ipvsas/mbp:latest
